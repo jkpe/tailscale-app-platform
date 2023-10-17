4f9d9e5ee5fe8426cab41589395185431f94217c
Jack Pearce <110543194+jkpedo@users.noreply.github.com>
2023-10-17 10:29:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=digitalocean-app-platform
-/app/bin/hello
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=do-app-platform
+echo Tailscale started
+ALL_PROXY=socks5://localhost:1055/ /usr/local/bin/hello
